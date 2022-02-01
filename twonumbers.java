e60ec516facc58b8b40d7b7cef3c59ba1f5ff2e6
kinthadaprasanna <95524490+kinthadaprasanna@users.noreply.github.com>
2022-02-01 16:36:17 +0530

    first commit

---
+++ twonumbers.java
@@ -0,0 +1,9 @@
+public class Myclass{
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cpublic class Myclass{
+     public static void main()[Dstring args[]){
+               [D[D[D[D[D[D[Dint x=10;
+        int y=25;
+        int z=x+y;
+
+        system[D[D[D[D[S[C[C[C[C[C.out.println()[D"Sum of x+y = " + z);
+ }
